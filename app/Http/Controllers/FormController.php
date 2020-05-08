3642a47a482f31c18f78c8b0a912fd8f3eeb3e72
Eugene Pro <eugenepro@icloud.com>
2020-05-08 15:09:07 +0300

    update

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -17,6 +17,7 @@ class FormController extends Controller
 
     public function store(FormValidator $request, $id)
     {
+//        dd($request->all(), $id);
         foreach($request->except(['_token', 'checkbox']) as $key => $value) {
             $field_id = explode('-', $key);
             Form::create([
@@ -26,7 +27,8 @@ class FormController extends Controller
             ]);
         }
 
-        return redirect()->route('form');
+        $id = ($id+1);
+        return redirect()->route('form', $id);
     }
 
     public function destroy($id)
